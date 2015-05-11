0d2e8ec248c593361c3e62e4fa6af34e27412e5f
g12909hr <g12909hr@m39908.tsuda.ac.jp>
2015-05-11 14:00:30 +0900

    ドライバー日高、ナビゲーター泰 リファクタリングを使いリネーム 1000〜10000の英訳

--- src/IntToEng/IntToEngTest.java
+++ src/IntToEng/IntToEngTest.java
@@ -63,9 +63,58 @@ public class IntToEngTest {
 	}
 	
 	@Test
-	public void int1000‚ð‘ã“ü‚µ‚½‚çthousand‚ð•Ô‚·() {
-		String expected = "thousand";
+	public void int1000‚ð‘ã“ü‚µ‚½‚çonethousand‚ð•Ô‚·() {
+		String expected = "one thousand";
 		String actual = IntToEng.translateEng(1000);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void int1019‚ð‘ã“ü‚µ‚½‚çonethousandnineteen‚ð•Ô‚·() {
+		String expected = "one thousand nineteen";
+		String actual = IntToEng.translateEng(1019);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void int1020‚ð‘ã“ü‚µ‚½‚çonethousandtwenty‚ð•Ô‚·() {
+		String expected = "one thousand twenty";
+		String actual = IntToEng.translateEng(1020);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void int1100‚ð‘ã“ü‚µ‚½‚çonethousandonehundred‚ð•Ô‚·() {
+		String expected = "one thousand one hundred";
+		String actual = IntToEng.translateEng(1100);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void int1119‚ð‘ã“ü‚µ‚½‚çonethousandonehundrednineteen‚ð•Ô‚·() {
+		String expected = "one thousand one hundred nineteen";
+		String actual = IntToEng.translateEng(1119);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void int1120‚ð‘ã“ü‚µ‚½‚çonethousandonehundredtwenty‚ð•Ô‚·() {
+		String expected = "one thousand one hundred twenty";
+		String actual = IntToEng.translateEng(1120);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void int1121‚ð‘ã“ü‚µ‚½‚çonethousandonehundredtwentyone‚ð•Ô‚·() {
+		String expected = "one thousand one hundred twenty one";
+		String actual = IntToEng.translateEng(1121);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void int10000‚ð‘ã“ü‚µ‚½‚çtenthousand‚ð•Ô‚·() {
+		String expected = "ten thousand";
+		String actual = IntToEng.translateEng(10000);
 		assertThat(actual, is(expected));
 	}
 }
